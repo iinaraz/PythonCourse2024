23280840b71c64ee40e44d60e263e6231d1c82b9
Iina Raz <iinaemilia.raz@gmail.com>
2024-11-11 12:12:43 +0200

    Add files via upload

---
+++ day02/circle.py
@@ -0,0 +1,9 @@
+# Circle area program
+
+import math
+
+# Define variables
+# radius = int(input("Radius:"))
+
+def calculate_circle(radius):
+    return("Area of the circle:", math.pi * radius**2, "Circumference of the circle:", 2*math.pi*radius)
---
+++ day02/rectangle.py
@@ -0,0 +1,11 @@
+# Rectangle area program
+
+# Define variables
+# width = int(input("Width:"))
+# length = int(input("Length:"))
+
+# Print
+def calculate_rectangle(width, length):
+    return("Area of a rectangle:", width * length, "Circumference of a rectangle:", width*2+length*2)
+
+
