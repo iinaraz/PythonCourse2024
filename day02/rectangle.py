07fea506cb7f9dc9c9aa4e5f42f7bb7ab5bfbfcf
Iina Raz <iinaemilia.raz@gmail.com>
2024-11-13 09:33:17 +0200

    Update rectangle.py

--- day02/rectangle.py
+++ day02/rectangle.py
@@ -1,11 +1,15 @@
 # Rectangle area program
 
 # Define variables
-# width = int(input("Width:"))
-# length = int(input("Length:"))
+width = int(input("Width:"))
+length = int(input("Length:"))
 
-# Print
+# Define function
 def calculate_rectangle(width, length):
     return("Area of a rectangle:", width * length, "Circumference of a rectangle:", width*2+length*2)
 
+# Save answer to a variable
+answer = calculate_rectangle(width, length)
 
+# Print answer
+print(answer)
