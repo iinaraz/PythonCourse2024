8679deb1e33a3709f12a263927a11fcb9e5d9871
Iina Raz <iinaemilia.raz@gmail.com>
2024-11-13 09:34:03 +0200

    Update circle.py

--- day02/circle.py
+++ day02/circle.py
@@ -5,5 +5,12 @@ import math
 # Define variables
 radius = int(input("Radius:"))
 
+# Define function
 def calculate_circle(radius):
     return("Area of the circle:", math.pi * radius**2, "Circumference of the circle:", 2*math.pi*radius)
+
+# Save answer to a variable
+answer = calculate_circle(radius)
+
+# Print answer
+print(answer)
