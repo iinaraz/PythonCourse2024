42e521bedff94b1ff3e5834a7aba12bcba74ef9f
Iina Raz <iinaemilia.raz@gmail.com>
2024-11-18 12:14:41 +0200

    more fixes

--- day03/circle.py
+++ day03/circle.py
@@ -8,9 +8,3 @@ def calculate_circle():
     radius = int(input("Radius:"))
 
     return("Area of the circle:", math.pi * radius**2, "Circumference of the circle:", 2*math.pi*radius)
-
-# Save answer to a variable
-answer = calculate_circle()
-
-# Print answer
-print(answer)
--- day03/rectangle.py
+++ day03/rectangle.py
@@ -7,9 +7,3 @@ def calculate_rectangle():
     length = int(input("Length:"))
 
     return("Area of a rectangle:", width * length, "Circumference of a rectangle:", width*2+length*2)
-
-# Save answer to a variable
-answer = calculate_rectangle()
-
-# Print answer
-print(answer)
